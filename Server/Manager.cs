555547088e7326f60ca2f561023caf4d92ec1cfa
potato1028 <ghgh10288@naver.com>
2023-09-11 13:24:32 +0900

    Photon

--- Server/Manager.cs
+++ Server/Manager.cs
@@ -3,16 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 public class Manager : MonoBehaviourPunCallbacks {
+    public Text StatusText;
+    public InputField NickNameInput;
+    public GameObject Cube;
+    public string gameVersion = "0.1";
+
+    void Awake() {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
     void Start() {
-        Screen.SetResolution(1080, 1920, false);
+        PhotonNetwork.GameVersion = this.gameVersion;
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    public override void OnConnectedToMaster() {
-        RoomOptions options = new RoomOptions();
-        options.MaxPlayers = 5;
-        PhotonNetwork.JoinOrCreateRoom("Room1", options, null);
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    override public void OnConnectedToMaster() {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    override public void OnJoinedRoom() {
+        PhotonNetwork.Instantiate("Player2", Cube.transform.position, Quaternion.identity);
+    }
+
+    override public void OnJoinRandomFailed(short returnCode, string message) {
+        this.CreateRoom();
+    }
+
+    public void CreateRoom() {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 5 });
     }
 }
