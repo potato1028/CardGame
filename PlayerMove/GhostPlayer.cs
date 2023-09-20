b94dd7b82eeec7aea43eabe636cd1d8ad572b130
potato1028 <ghgh10288@naver.com>
2023-09-21 02:23:09 +0900

    서버 하는중

---
+++ PlayerMove/GhostPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPlayer : MonoBehaviour {
+    void Start() {
+        Move();
+    }
+
+    void Move() {
+        
+    }
+}
